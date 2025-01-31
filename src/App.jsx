173401a7c0e587f676f56e784fe4aa3c73ba90c0
Jaggo <jaggolap@hotmailcom>
2025-01-31 10:01:29 -0300

    Ultima review

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import HomePage from './pages/HomePage';
 import EmpleadoPage from './pages/EmployeePage';
 import RegisterPage from './pages/RegisterPage';
 import ClientePage from './pages/ClientPage';
-
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/RegisterPage';
-import EmployeeDashboard from './pages/EmployeeDashboard';
 import MemberDashboard from './pages/MemberDashboard';
 import { AuthProvider } from './contexts/AuthContext';
 
@@ -18,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cliente" element={<ClientePage />} />
-        <Route path="/Login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/empleado" element={<EmpleadoPage />} />
       </Routes>
